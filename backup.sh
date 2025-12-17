585b2b6db629162be09246fef6467c4f35417d86
testDenis7788 <testemaildenis@proton.me>
2025-12-17 12:07:19 +0100

    UPDATE backup.sh

--- backup.sh
+++ backup.sh
@@ -1,69 +1,45 @@
 #!/bin/bash
 
-# This checks if the number of arguments is correct
-# If the number of arguments is incorrect ( $# != 2) print error message and exit
-if [[ $# != 2 ]]
-then
-  echo "backup.sh target_directory_name destination_directory_name"
+# Check arguments
+if [[ $# != 2 ]]; then
+  echo "Usage: backup.sh target_directory_name destination_directory_name"
   exit
 fi
 
-# This checks if argument 1 and argument 2 are valid directory paths
-if [[ ! -d $1 ]] || [[ ! -d $2 ]]
-then
+# Validate directories
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]; then
   echo "Invalid directory path provided"
   exit
 fi
 
-# [TASK 1]
+# Set variables
 targetDirectory=$1
 destinationDirectory=$2
-
-# [TASK 2]
-echo "Target directory is $targetDirectory"
-echo "Destination Directory is $destinationDirectory" 
-
-# [TASK 3]
 currentTS=$(date +%s)
-
-# [TASK 4]
 backupFileName="backup-$currentTS.tar.gz"
-
-# We're going to:
-  # 1: Go into the target directory
-  # 2: Create the backup file
-  # 3: Move the backup file to the destination directory
-
-# To make things easier, we will define some useful variables...
-
-# [TASK 5]
 origAbsPath=$(pwd)
 
-# [TASK 6]
+# Get absolute path of destination directory
 cd "$destinationDirectory" || exit
 destAbsPath=$(pwd)
 
-# [TASK 7]
-cd "$origAbsPath" || exit 
+# Go back to target directory
+cd "$origAbsPath" || exit
 cd "$targetDirectory" || exit
 
-# [TASK 8]
+# Timestamp for 24 hours ago
 yesterdayTS=$(($currentTS - 24 * 60 * 60))
 
+# Find files modified in the last 24 hours
 declare -a toBackup
-
-for file in * # [TASK 9]
-do
-  # [TASK 10]
-  if [[ `date -r "$file" +%s` -gt $yesterdayTS ]]
-  then
-    # [TASK 11]
-    toBackup+=($file)
+for file in *; do
+  if [[ `date -r "$file" +%s` -gt $yesterdayTS ]]; then
+    toBackup+=("$file")
   fi
 done
 
-# [TASK 12]
-tar -czvf $backupFileName ${toBackup[@]}
-# [TASK 13]
+# Create backup archive
+tar -czvf "$backupFileName" "${toBackup[@]}"
+
+# Move archive to destination directory
 mv "$backupFileName" "$destAbsPath"
-# Congratulations! You completed the final project for this course!
